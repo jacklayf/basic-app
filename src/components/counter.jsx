efbe456d92b1f3042aad95bcd62a754304763fb3
jackml <jackml@vt.edu>
2021-07-05 17:09:06 -0700

    testing with lists

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,7 +3,7 @@ import React, { Component } from "react";
 class Counter extends Component {
   state = {
     count: 0,
-    tags: ["tag1", "tag2", "tag3"],
+    tags: [],
   };
 
   //styles = {
@@ -11,29 +11,25 @@ class Counter extends Component {
   // fontWeight: "bold",
   //};
 
-  render() {
+  renderTags() {
+    if (this.state.tags.length === 0) return <p>There are no tags!</p>;
+
     return (
-      <div>
-        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-        <button className="btn btn-secondary btn-sm">Increment</button>
-        <ul>
-          {this.state.tags.map((tag) => (
-            <li key={tag}>{tag}</li>
-          ))}
-        </ul>
-      </div>
+      <ul>
+        {this.state.tags.map((tag) => (
+          <li key={tag}>{tag}</li>
+        ))}
+      </ul>
     );
   }
 
-  getBadgeClasses() {
-    let classes = "badge m-2 ";
-    classes += this.state.count === 0 ? "badge-warning" : "badge-primary";
-    return classes;
-  }
-
-  formatCount() {
-    const { count } = this.state;
-    return count === 0 ? "Zero" : count;
+  render() {
+    return (
+      <div>
+        {this.state.tags.length === 0 && "Please create a new tag!"}
+        {this.renderTags()}
+      </div>
+    );
   }
 }
 
